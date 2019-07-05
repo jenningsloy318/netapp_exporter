01bd29623eb4786b8fd2c99f57ff1b0f6152fce1
Jennings Liu <jennings.liu@sap.com>
2019-07-05 10:16:27 +0800

    add fan/powersupply/temperature metrics

--- collector/system.go
+++ collector/system.go
@@ -21,7 +21,20 @@ var (
 	systemNodeUptimeDesc = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, SystemSubsystem, "uptime"),
 		"uptime of the node.",
-		[]string{"name", "model", "location", "productversion", "uuid"}, nil)
+		[]string{"name", "model", "location", "uuid"}, nil)
+	systemNodeFailedFanCountDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, SystemSubsystem, "failed_fan_count"),
+		"Failed Fan Count of the node.",
+		[]string{"name", "model", "location", "uuid"}, nil)
+	systemNodeFailedPowerSupplyCountDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, SystemSubsystem, "failed_powersupply_count"),
+		"Failed PowerSupply Count of the node.",
+		[]string{"name", "model", "location", "uuid"}, nil)
+	systemNodeOverTemperatureDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, SystemSubsystem, "over_temperature"),
+		"Over Temperature of the node.",
+		[]string{"name", "model", "location", "uuid"}, nil)
+
 )
 
 
@@ -40,13 +53,16 @@ func (ScrapeSystem) Help() string {
 
 
 type Node struct {
-	Name                string
-	OwnerName           string
-	Uptime							string
-	Model               string
-	Location 						string
-	Productversion      string
-	Uuid								string
+	Name                				string
+	OwnerName           				string
+	Model               				string
+	Location 										string
+	Uuid												string
+	Uptime											string
+	EnvFailedFanCount   				int
+	EnvFailedPowerSupplyCount   int 
+	EnvOverTemperature          bool 
+
 }
 
 
@@ -55,8 +71,13 @@ func (ScrapeSystem) Scrape(netappClient *netapp.Client, ch chan<- prometheus.Met
 
 	for _, NodeInfo := range GetNodeData(netappClient) {
 		if uptime,err :=  strconv.ParseFloat(NodeInfo.Uptime, 64); err == nil{
-			ch <- prometheus.MustNewConstMetric(systemNodeUptimeDesc, prometheus.GaugeValue,uptime, NodeInfo.Name, NodeInfo.Model, NodeInfo.Location, NodeInfo.Productversion, NodeInfo.Uuid)
+			ch <- prometheus.MustNewConstMetric(systemNodeUptimeDesc, prometheus.GaugeValue,uptime, NodeInfo.Name, NodeInfo.Model, NodeInfo.Location, NodeInfo.Uuid)
 	 }
+	 ch <- prometheus.MustNewConstMetric(systemNodeFailedFanCountDesc, prometheus.GaugeValue,float64(NodeInfo.EnvFailedFanCount), NodeInfo.Name, NodeInfo.Model, NodeInfo.Location, NodeInfo.Uuid)
+	 ch <- prometheus.MustNewConstMetric(systemNodeFailedPowerSupplyCountDesc, prometheus.GaugeValue,float64(NodeInfo.EnvFailedPowerSupplyCount), NodeInfo.Name, NodeInfo.Model, NodeInfo.Location, NodeInfo.Uuid)
+	 var b2f = map[bool]float64{false: 0, true: 1}
+	 ch <- prometheus.MustNewConstMetric(systemNodeOverTemperatureDesc, prometheus.GaugeValue,b2f[NodeInfo.EnvOverTemperature], NodeInfo.Name, NodeInfo.Model, NodeInfo.Location, NodeInfo.Uuid)
+	 
 	}
 	return nil
 }
@@ -77,13 +98,15 @@ func GetNodeData(netappClient *netapp.Client) (r []*Node) {
 
 	for _, n := range l {
 		r = append(r, &Node{
-			Name:                n.Name,
-			OwnerName:           n.NodeOwner,
-			Uptime:						   n.NodeUptime,
-			Model:               n.NodeModel,
-			Location: 					 n.NodeLocation,
-			Productversion:      n.ProductVersion,
-			Uuid:								 n.NodeUuid,
+			Name:                			 n.Name,
+			OwnerName:           			 n.NodeOwner,
+			Model:               			 n.NodeModel,
+			Location: 					 			 n.NodeLocation,
+			Uuid:								 			 n.NodeUuid,
+			Uptime:						   			 n.NodeUptime,
+			EnvFailedFanCount:   			 n.EnvFailedFanCount,
+			EnvFailedPowerSupplyCount: n.EnvFailedPowerSupplyCount,
+			EnvOverTemperature:				 n.EnvOverTemperature,
 		})
 	}
 	return
